ba3cc4760d43634f1005aba9b49e794aed3e1248
Oliver Moody <omoody@vassar.edu>
2021-05-17 20:33:40 -0400

    testing doc

--- rotowire/model_2/testmodel/make_test_sentences.py
+++ rotowire/model_2/testmodel/make_test_sentences.py
@@ -1,12 +1,12 @@
 import json
-with open('../../model_1/tagged_test_summaries.txt','r') as fr:
+with open('../../model_1/tagged_test_summaries.txt','r') as fr: #get tagged summaries
         d = fr.read()
         full_dict = json.loads(d)
 
 stat_sums = []
 for s in full_dict:
-    if s[-3] or s[-2] or s[-1]:
-        stat_sums.append(s[:-3])
+    if s[-3] or s[-2] or s[-1]: #if they have a stat
+        stat_sums.append(s[:-3]) #include without dict
 
-with open('stat_sentences_test.txt','w') as fw: 
+with open('stat_sentences_test.txt','w') as fw: #dump
 	sum_json = json.dump(stat_sums,fw)
--- rotowire/model_2/testmodel/runtest.py
+++ rotowire/model_2/testmodel/runtest.py
@@ -1,29 +1,33 @@
 import json
 import random
 
-with open('stat_sentences_test.txt','r') as fr:
+with open('stat_sentences_test.txt','r') as fr: #get real sentences
 	d = fr.read()
 	real = json.loads(d)
 
-with open('../results.txt','r') as fr:
+with open('../results.txt','r') as fr: #get generated sentences
 	generated = fr.readlines()
+
+#make both lowercase
 real = [[i.lower() for i in sent] for sent in real]
 generated = [i.lower().split() for i in generated]
 
+#shuffle
 random.shuffle(real)
 random.shuffle(generated)
 
 # print(real[:2],generated[:2])
+
 correct = 0
-for i in range(25):
-	sents = [" ".join(real[i]), " ".join(generated[i])]
+for i in range(25): #do 25 trials
+	sents = [" ".join(real[i]), " ".join(generated[i])] #join for readability
 	#print(sents)
 	real_ind = 1
-	if random.randint(0,1)==1:
+	if random.randint(0,1)==1: # random order
 		sents = sents[::-1]
 		real_ind = 2
 	print(sents)
 	s= input("{}. Guess the real sentence (1 or 2) ".format(i+1))
 	if s==str(real_ind):
 		correct+=1
-print("You guessed",correct,"out of 25")
+print("You guessed",correct,"out of 25") #print results
